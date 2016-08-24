e5d4326ad154d6ca000dab038c6383f36b8e40ed
aasirjesudas <singhdas25@gmail.com>
2016-08-24 19:58:36 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ int n,i;
+		Scanner s=new Scanner(System.in);
+		int[] a1=new int[n];
+		int n=s.next();
+		for( i=0;i<n;i++)
+		{
+		a1[i]=s.next();
+		}
+		Arrays.sort(a1);
+		if(a[i]==i)
+		{
+		  System.out.println(i);
+		}
+		else
+		System.out.println("no");
+	}
+}
