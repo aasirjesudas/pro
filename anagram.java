2ac7ce18547f34fe59869a013e9bd189840aa50a
aasirjesudas <singhdas25@gmail.com>
2016-08-24 22:18:58 +0530

    Create anagram.java

---
+++ anagram.java
@@ -0,0 +1,21 @@
+public class a
+{
+public static void main(string[] args)
+{
+Sacnner s=new Scanner(System.in);
+String ch=s.next();
+String ch1=s.next();
+char a1[]=ch.toLowerCase.toCharArray();
+char b1[]=ch1.toLowercase.toCharArray();
+if(a1.length==b1.length)
+{
+Arrays.sort(a1);
+arrays.sort(b1);
+if(Arrays.equals(a1,b1));
+{
+System.out.println("anagram");
+} else{
+System.out.println("not anagram");
+}}
+else
+System.out.println("length ineuality");
