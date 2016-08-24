08ad51980fb3df38114c008135abafdfb0acaa0d
aasirjesudas <singhdas25@gmail.com>
2016-08-24 22:23:26 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Test{
+   public static void main(String args[]){
+   	int n;
+   	Sanner s=new Scanner(System.in);
+      String S = s.NextLine();
+      n=str.length();
+      System.out.print("Return Value :" );
+      System.out.println(S.substring(n/2, n-(n/2)) );
+   }
+}
